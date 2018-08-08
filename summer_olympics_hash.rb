93860d209f7e4e906d418cc5a566a41911371d0e
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-08 20:36:25 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -13,13 +13,13 @@ end
 def add_a_key_value_pair
   summer_olympics = create_olympics_hash
   summer_olympics[:Atlanta] = "1996"
-  # summer_olympics
+  summer_olympics
 end
 
 def iterate_through_hash
   summer_olympics = add_a_key_value_pair
   summer_olympics.each do |x,y|
-    puts "The #{x} summer olympics took place in #{y}"
+    puts "The #{x} summer olympics took place in #{y}."
   end
 end
 
